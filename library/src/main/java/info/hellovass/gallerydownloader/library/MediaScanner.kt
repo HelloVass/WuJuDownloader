bdd3705becd3120abe516deaf88be733e9afc827
hellovass <hellova33@gmail.com>
2018-08-31 21:39:09 +0800

    fix memory issue

--- library/src/main/java/info/hellovass/gallerydownloader/library/MediaScanner.kt
+++ library/src/main/java/info/hellovass/gallerydownloader/library/MediaScanner.kt
@@ -8,11 +8,13 @@ class MediaScanner {
 
     companion object {
 
-        fun scanDir(context: Context, dirPath: String) {
-            MediaScannerConnection.scanFile(context, getFiles(dirPath), null, null)
-        }
+        fun scanDir(context: Context, dirPath: String) =
+                MediaScannerConnection.scanFile(context.applicationContext, listDirFilePath(dirPath), null, null)
 
-        private fun getFiles(dirPath: String): Array<String> {
+        /**
+         * get specific dir's file path
+         */
+        private fun listDirFilePath(dirPath: String): Array<String> {
 
             return File(dirPath).list()
                     .map { filePath -> "$dirPath${File.separator}$filePath" }
